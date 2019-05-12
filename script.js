bff297c78761e9f936846ba9a144091fc793da5a
Param Thakkar <pht0104@gmail.com>
2019-05-12 14:03:24 -0400

    Add smooth transition to "Back to full website"

--- script.js
+++ script.js
@@ -25,4 +25,12 @@ function hover(state, i){
             }
             break;
     }
-} 
+}
+
+function redirectToHome(){
+    document.getElementById("transition-overlay").style.opacity = "1";
+    document.getElementById("transition-overlay").style.zIndex = "2";
+    setTimeout(function(){
+      window.location='/';
+    }, 1000);
+  }
