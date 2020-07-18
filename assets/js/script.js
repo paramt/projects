2aa967eb6d288d476659e9a4600b69a7f24517a8
Param Thakkar <pht0104@gmail.com>
2020-07-18 17:53:47 -0400

    Add bouncing arrow to prompt scrolling

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,20 @@ function redirectToHome(){
       window.location='/';
     }, 1000);
 }
+
+// Reveal bouncing arrow
+window.setTimeout(function(){
+  document.getElementById("arrow").style.opacity = "1";
+  document.getElementById("arrow").classList = "animated bounce";
+}, 100)
+
+// Hide arrow
+window.addEventListener("scroll", function(){
+  var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+  if(scrollTop != 0){
+    document.getElementById("arrow").classList = "animated fadeOutUp";
+  } else {
+    document.getElementById("arrow").classList = "animated bounce";
+  }
+}, false);
